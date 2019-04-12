8ba67d48b8355a584fbe80c01f004334d3df39a9
Gray Rybka <grybka@uw.edu>
2019-04-12 12:36:39 -0700

    now works

--- local_dragonfly/custom_homeputer_dragonfly/PM25Spime.py
+++ local_dragonfly/custom_homeputer_dragonfly/PM25Spime.py
@@ -22,12 +22,17 @@ class PM25Spime(Spime):
 
     def on_get(self):
         #I suppose I'll have it reconnect on every get.  Seems a little excessive
+        print("connecting to serial")
+        #led = DigitalInOut(board.D13)
+        #led.direction = Direction.OUTPUT
         uart=serial.Serial("/dev/ttyS0",baudrate=9600,timeout=3000)
         for cycle in range(100):
+            print("reading some data")
             buffer = []
             data=uart.read(32)
             data=list(data)
             buffer+=data
+            print("buffer length now {}".format(len(buffer)))
             while buffer and buffer[0] != 0x42:
                 buffer.pop(0)
             if len(buffer) > 200:
@@ -38,7 +43,6 @@ class PM25Spime(Spime):
                 buffer.pop(0)
                 continue
             #raise exceptions.DriplineHardwareError('error, didnt get 32 bytes of data')
-            return []
             frame_len=struct.unpack(">H",bytes(buffer[2:4]))[0]
             if frame_len != 28:
                 buffer =[]
@@ -53,7 +57,7 @@ class PM25Spime(Spime):
                 print("checksum failed")
                 raise exceptions.DriplineHardwareError('error, checksum failed')
                 return []
-            return [pm10_en,pm25_env,pm100_env,particles_03um,particles_05um,particles_10um,particles_25um,particles_50um,particles_100um]
+            return [pm10_env,pm25_env,pm100_env,particles_03um,particles_05um,particles_10um,particles_25um,particles_50um,particles_100um]
         raise exceptions.DriplineHardwareError('communication to pm25 sensor failed somehow')
     
 __all__.append('PM25Spime')
